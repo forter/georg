19b4b38fb37fc58241c90abfcb14fc3778214631
Yaniv Nikankin <yanivnikan@gmail.com>
2014-06-16 09:35:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ var exceptions = require('./lib/exceptions.js'),
 ///featureList is an array of strings, each representing a feature, i.e. - ['exceptions', 'latencies'].
 ///config is a configuration object for the riemann connection, i.e. - {host: '127.0.0.1', port: 5555, service: 'brainA'}.
 exports.init = function(featureArray, config) {
-    //CONNECT TO RIEMANN
+    //Connect To Riemann
     connection.connectToRiemann(config);
 
     //Call the starter methods for each feature in the feature array
     //and export all needed functions for each feature
-    //TODO: FIND A BETTER WAY TO DO THIS, PERHAPS USING A MAP{string, function}
     if (featureArray.indexOf('exceptions') > -1) {
         exceptions.catchExceptions(config.service);
     }
@@ -20,16 +19,3 @@ exports.init = function(featureArray, config) {
         exports.recordLatency = latencies.recordLatency;
     }
 };
-
-
-
-/*TODO: THE MAP(string, function) code will look like this -
- var a = {};
- a["exceptions"] = function() { console.log("EXCEPTIONS")};
- a["latencies"] = function() { console.log("LATENCIES")};
-
- var flist = ['exceptions', 'latencies'];
-
- for(var f in flist)
- a[flist[f]]();
- */
