a7d580dc4763e6ebc75b57465f9b4222440e2750
YanivNikankin <yanivn@forter.com>
2014-07-14 11:19:37 +0300

    Added exception-suppress feature

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var exceptions = require('./lib/exceptions.js'),
     latencies = require('./lib/latencies.js'),
-    connection = require('./lib/riemannConnection.js');
+    connection = require('./lib/riemannConnection.js'),
+    suppress = require('./suppress.js');
 
 
 ///This method should be called once, at one point in the code, after require('georg');
@@ -12,6 +13,7 @@ exports.init = function(config) {
     //Call the starter methods for each feature in the feature array
     //and export all needed functions for each feature
     if (!!config.exceptions) {
+        exports.Suppressor = suppress.Suppressor;
         exceptions.catchExceptions(config.exceptions, config.service);
     }
     if (config.latencies) {
