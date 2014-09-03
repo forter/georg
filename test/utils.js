30c7be6658e4d063ce76775c8a9890b8e633a65d
anya <anya@forter.com>
2014-09-03 11:53:24 +0300

    Fix custom attributes format

--- test/utils.js
+++ test/utils.js
@@ -8,11 +8,12 @@ var should = require('chai').should();
 
 describe('Check convert json to riemann format', function() {
     it('for json should return list of key value pairs', function() {
-        utils.customAttributesToRiemannFormat({"foo": "bar", "baz": 2}).should.deep.include.members([{'foo': 'bar'}, {'baz': 2}]);
+        utils.customAttributesToRiemannFormat({"foo": "bar", "baz": 2}).should.deep.
+            include.members([{key: 'foo', value: 'bar'}, {key: 'baz', value: 2}]);
     })
 
     it('for non object should return undefined', function () {
-        should.not.exist(utils.customAttributesToRiemannFormat("a"))
-        should.not.exist(utils.customAttributesToRiemannFormat())
+        utils.customAttributesToRiemannFormat("a").should.be.empty;
+        utils.customAttributesToRiemannFormat().should.be.empty;
     })
 })
