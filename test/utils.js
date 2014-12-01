d88323a0562bd27e9bb848634ee9b31bbaa01e70
anya <anya@forter.com>
2014-12-01 11:24:38 +0200

    Use _ instead of implementing object merge. Add test

--- test/utils.js
+++ test/utils.js
@@ -1,13 +1,13 @@
 
 var utils = require('../lib/utils.js');
+var mandatoryAttributes = require('../lib/mandatoryAttributes');
 var chai = require('chai');
 var should = chai.should();
 chai.use(require('chai-fuzzy'));
 
 describe('Check convert json to riemann format', function() {
     it('for json should return list of key value pairs', function() {
-        utils.customAttributesToRiemannFormat({"foo": "bar", "baz": 2}).should.deep.
-            include.members([{key: 'foo', value: 'bar'}, {key: 'baz', value: 2}]);
+        utils.customAttributesToRiemannFormat({"foo": "bar", "baz": 2}).should.eql([{key: 'foo', value: 'bar'}, {key: 'baz', value: 2}]);
     })
 
     it('for non object or empty object should return empty list', function () {
@@ -18,26 +18,19 @@ describe('Check convert json to riemann format', function() {
 })
 
 
-describe('Check join objects', function() {
-    it('Should return objects containing all attributes of both of the inputs', function() {
-        utils.joinObjects({"foo": "bar"}, {"baz": 1}).should.be.jsonOf({"foo": "bar", "baz": 1});
-        utils.joinObjects({}, {}).should.be.jsonOf({});
-        utils.joinObjects({"foo": "bar"}, {}).should.be.jsonOf({"foo": "bar"});
-        utils.joinObjects({}, {"baz": 1}).should.be.jsonOf({"baz": 1});
-    })
-
-    it('should not change original onjects', function() {
-        var obj1 = {"foo": "bar", "baz": 1};
-        var obj2 = {"bar": 2};
-        utils.joinObjects(obj1, obj2);
-        obj1.should.be.jsonOf({"foo": "bar", "baz": 1});
-        obj2.should.be.jsonOf({"bar": 2});
-    })
-})
-
 describe('Test getAllAttributesRiemannFormat', function() {
-    var mandatoryAtts = {"foo": "bar", "baz": 2};
-    it('should return given attributes + mandatory attributes in riemann format', function() {
+    var mandatoryAttrs = {"foo": "bar", "baz": 2};
 
+    it('should return given attributes + mandatory attributes in riemann format', function() {
+        mandatoryAttributes.init(mandatoryAttrs);
+        var customAttrs = {"att1": 1};
+        var allAttrs = utils.getAllAttributesRiemannFormat(customAttrs);
+        allAttrs.should.eql([{key: 'att1', value: 1}, {key: 'foo', value: 'bar'}, {key: 'baz', value: 2}]);
+    })
+
+    it('should return only mandatory attributes for empty json', function() {
+        mandatoryAttributes.init(mandatoryAttrs);
+        var allAttrs = utils.getAllAttributesRiemannFormat({});
+        allAttrs.should.eql([{key: 'foo', value: 'bar'}, {key: 'baz', value: 2}]);
     })
 })
